066e37f8638c37cf7af4a0187c76604462a7f62e
ceasar0524 <ceasar0524@gmail.com>
2026-03-23 21:11:24 +0800

    fix: throw on Resend API error instead of silently ignoring

--- notification/mailer.js
+++ notification/mailer.js
@@ -4,13 +4,15 @@ const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendMail({ to, subject, text, html }) {
-  await resend.emails.send({
+  const { data, error } = await resend.emails.send({
     from: process.env.SMTP_FROM || '如如的創意料理 <onboarding@resend.dev>',
     to,
     subject,
     text,
     html,
   });
+  if (error) throw new Error(JSON.stringify(error));
+  console.log('[mailer] sent:', data?.id, 'to:', to);
 }
 
 module.exports = { sendMail };
